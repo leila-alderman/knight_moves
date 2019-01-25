bd322e1183884908d2e37d41586683a9435c75bc
leila-alderman <leila.alderman@gmail.com>
2019-01-25 12:25:23 -0500

    Add Board class

--- lib/knight_moves.rb
+++ lib/knight_moves.rb
@@ -1,5 +1,15 @@
 class Board
+    attr_accessor :spaces
+
     def initialize
+        @spaces = []
+        for i in "A".ord.."H".ord
+            @row = []
+            for j in 1..8
+                @row.push([i.chr, j])
+            end
+            @spaces.push(@row)
+        end
     end
 end
 
@@ -8,6 +18,6 @@ class Knight
         @position = [0,0]
     end
 
-    def knight_moves(start, end)
+    def knight_moves(start, stop)
     end
 end
--- spec/knight_moves_spec.rb
+++ spec/knight_moves_spec.rb
@@ -1,5 +1,8 @@
 require "knight_moves"
 
-describe "Board"
-
+describe "Board" do
+	it "initializes a board" do
+		board = Board.new
+        expect(board.spaces[0][0]).to eql(["A", 1])
+	end
 end
